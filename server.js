11c0f856664c4d0e7a0bca1e6363356b3eb20051
david  hoffmann <davidhoffmann@davids-MacBook-Pro.local>
2018-08-05 12:18:00 -0500

    server updates

--- server.js
+++ server.js
@@ -14,3 +14,30 @@ app.get("/", function(req, res) {
 app.listen(PORT, function() {
   console.log("App listening on PORT " + PORT);
 });
+
+var nodemailer = require('nodemailer');
+
+
+var email = document.getElementById(reserve_uniqueID);
+var transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: 'hotrest123@gmail.com',
+    pass: '1111asdf'
+  }
+});
+
+var mailOptions = {
+  from: 'hotrest123@gmail.com',
+  to: email,
+  subject: 'Table Ready!',
+  text: 'Thank you for your patience. Your table is ready! Please see the the hostess to be seated.'
+};
+
+transporter.sendMail(mailOptions, function(error, info){
+  if (error) {
+    console.log(error);
+  } else {
+    console.log('Email sent: ' + info.response);
+  }
+});
